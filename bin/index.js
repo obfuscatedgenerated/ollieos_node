5a2f811108c6b4fa38f68b9d35d982ac9edcf03b
Ollie <75035364+obfuscatedgenerated@users.noreply.github.com>
2025-06-19 03:24:02 +0100

    there was indeed a cleaner way

--- bin/index.js
+++ bin/index.js
@@ -6,12 +6,8 @@ const path = require("path");
 const root = path.resolve(__dirname, "..");
 const args = process.argv.slice(2);
 
-// determine script based on if passed --no-clear
-// yes, we could just run tsx directly here and have it pass the argv, but if we change how npm start works, we'd have to change this too
-const script = args.includes("--no-clear") ? "start-no-clear" : "start";
-
 // run npm start in the project root
-const child = spawn("npm", ["run", script, ...args.slice(1)], {
+const child = spawn("npm", ["run", "start", "--", args], {
     cwd: root,
     stdio: "inherit",
     shell: true,
