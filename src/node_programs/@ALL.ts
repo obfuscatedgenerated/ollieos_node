974691855c919dcec7eab0d4d9348b8a850b4ace
Ollie <75035364+obfuscatedgenerated@users.noreply.github.com>
2025-06-19 02:46:54 +0100

    os update command

--- src/node_programs/@ALL.ts
+++ src/node_programs/@ALL.ts
@@ -1,3 +1,4 @@
 // node specific tools injected to the registry at startup
 
 export { default as appdata } from "./appdata";
+export { default as update } from "./update";
---
+++ src/node_programs/update.ts
@@ -0,0 +1,60 @@
+import type { Program } from "ollieos/src/types";
+
+import { ANSI, NEWLINE } from "ollieos/src/term_ctl";
+
+import { exec } from "node:child_process";
+import { promisify } from "node:util";
+import path from "node:path";
+
+export default {
+    name: "update",
+    description: "[Node tools] Updates the OS via npm.",
+    usage_suffix: "",
+    arg_descriptions: {},
+    main: async (data) => {
+        // extract from data to make code less verbose
+        const { term } = data;
+
+        // extract from ansi to make code less verbose
+        const { PREFABS, STYLE, FG } = ANSI;
+
+        // get real life working directory
+        const cwd = process.cwd();
+
+        // if working directory is not where the code lives, change it
+        const code = path.join(__dirname, "..", "..");
+        if (cwd !== code) {
+            process.chdir(code);
+        }
+
+        term.writeln("Updating OllieOS...");
+
+        // run the update command
+        const exec_async = promisify(exec);
+        try {
+            const { stdout, stderr } = await exec_async("npm update ollieos");
+
+            term.writeln(stdout);
+            if (stderr) {
+                term.writeln(`${PREFABS.error}${stderr}${STYLE.reset_all}`);
+            }
+
+            term.write(FG.green);
+            term.writeln("OllieOS updated successfully.");
+            term.writeln("Please restart the terminal to apply the changes.");
+            term.write(STYLE.reset_all);
+
+            // restore working directory
+            process.chdir(cwd);
+
+            return 0;
+        } catch (error) {
+            term.writeln(`${PREFABS.error}Error updating OllieOS:${NEWLINE}${error}${STYLE.reset_all}`);
+
+            // restore working directory
+            process.chdir(cwd);
+
+            return 1;
+        }
+    }
+} as Program;
