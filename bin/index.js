83354c9d4ef3f6bac0e2f1ea2b3bade43621f15b
Ollie <75035364+obfuscatedgenerated@users.noreply.github.com>
2025-06-20 04:46:01 +0100

    fix security goof (shut up deprecationwarning)

--- bin/index.js
+++ bin/index.js
@@ -9,7 +9,7 @@ const args = process.argv.slice(2);
 // run npm start in the project root
 // use silent mode to suppress the > lines (to avoid breaking pipes and it looks neater)
 // and pass through the args to the script
-const child = spawn("npm", ["run", "start", "--silent", "--", args], {
+const child = spawn("npm", ["run", "start", "--silent", "--", ...args], {
     cwd: root,
     stdio: "inherit",
     shell: true,
