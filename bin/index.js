6cc6c4dad9c69c008d11d377603e282ca7200eaa
Ollie <75035364+obfuscatedgenerated@users.noreply.github.com>
2025-06-20 04:50:51 +0100

    really stop it whining

--- bin/index.js
+++ bin/index.js
@@ -9,7 +9,9 @@ const args = process.argv.slice(2);
 // run npm start in the project root
 // use silent mode to suppress the > lines (to avoid breaking pipes and it looks neater)
 // and pass through the args to the script
-const child = spawn("npm", ["run", "start", "--silent", "--", ...args], {
+// escape the arguments to stop node whining
+const command = `npm run start --silent -- ${args.map(a => `"${a}"`).join(" ")}`;
+const child = spawn(command, {
     cwd: root,
     stdio: "inherit",
     shell: true,
